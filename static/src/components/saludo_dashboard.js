c3960f53396c971e85c9348a5be03c7cace8ec6f
Martin <martin.brusco@gmail.com>
2025-04-13 20:29:24 +0200

    Segundo capitulo listo

--- static/src/components/saludo_dashboard.js
+++ static/src/components/saludo_dashboard.js
@@ -1,4 +1,4 @@
-/**@odoo-module */
+/** @odoo-module */
 
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
@@ -8,13 +8,47 @@ export class OwlSaludosDashboard extends Component {
     setup() {
         this.state = useState({
             title: "",
+            information: [],
+            countModule: [],
         });
+
+        this.orm = useService("orm");
 
         onWillStart(async () => {
             console.log("onWillStart");
             this.state.title = "Saludos Dashboard";
-            console.log("this.state.title", this.state.title);
+            this.state.information = await this.getModuleInformation();
+            this.state.countModule = await this.getModuleCount();
         });
+    }
+
+    async getModuleInformation() {
+        try {
+            const information = await this.orm.searchRead(
+                "ir.module.module",
+                [["name", "=", "saludos"]],
+                ["id", "author", "description", "shortdesc", "website", "summary"]
+            );
+            return information;
+        } catch (error) {
+            console.error("Error fetching module information:", error);
+            return [];
+        }
+    }
+
+    async getModuleCount() {
+        try {
+            const count = await this.orm.readGroup(
+                "ir.module.module",
+                [[]],
+                ["state"],
+                ["state"],
+            );
+            return count;
+        } catch (error) {
+            console.error("Error fetching module information:", error);
+            return [];
+        }
     }
 }
 
