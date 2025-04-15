3f1a491fb96788b517cc34df241e7590d0574112
Martin <martin.brusco@gmail.com>
2025-04-15 17:56:44 +0200

    capitulo 3 version completa

--- static/src/components/chart/chart_renderer.js
+++ static/src/components/chart/chart_renderer.js
@@ -8,36 +8,39 @@ export class OwlChartRenderer extends Component {
     setup() {
         this.chartRef= useRef('chart') 
         this.state= useState({
-            title: "Modules",
+            title: this.props.title,
+            type: this.props.type,
+            data: this.props.data
         });
         onWillStart(async () => {
             await loadJS("https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.js");
         })
         onMounted(() =>{
-            
-            console.log("ChatMount" );
-            this.render(this.state.title);
+
+            const labels= [];
+            const metrics=[];
+            this.state.data.map((item)=>{
+                labels.push(item.state);
+                metrics.push(item.state_count);
+            })
+            this.render(this.state.title, labels, metrics, this.state.type);
         });
     }
 
-    render(title){
+    render(title, labels, metrics, type='pie'){
         const Chart = window.Chart;  // Esto es necesario cuando usás Chart.js vía CDN
         new Chart(this.chartRef.el, {
-            type:'pie',
+            type,
             data: {
-                labels: ['Red', 'Blue', 'Yellow'],
+                labels: labels,
                 datasets:[{
                     label:title,
-                    data:[12, 19, 3]
+                    data:metrics
                 }]
             }
         });
     }
     
-
-
-
-
 }
 
 OwlChartRenderer.template= 'owl.OwlChartRenderer';
